613d74af2d14b61f29d89f6561eaa2e2e138b041
132118 <132118@IA22.labinfo.facens.br>
2017-02-15 22:16:43 -0200

    versao3

--- Teste150217/src/Teste/Teste.java
+++ Teste150217/src/Teste/Teste.java
@@ -1,10 +1,11 @@
 package Teste;
 
-//versao2
+//versao3
 public class Teste {
 	
 	public static void main(String [] args){
 	System.out.println("Hello World");
+	System.out.println("Mari tem um crush");
 }
 
 }
